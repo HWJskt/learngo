9ef46662d3aadc08e10870f5fbe69981bc544f5f
HWJskt <hyunwookj83@gmail.com>
2022-12-12 07:42:14 +0900

    finish

--- 11_UrlChecker/main.go
+++ 11_UrlChecker/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 func main() {
+	// empty map 을 만드는법
+	// var results map[string]string  // 이 map 은 nill 이 되어 이건 나중에 값을 넣을때 오류난다
+	var results = map[string]string{}
+	results["hello"] = "hello"
+
+	c := make(chan requestResult)
+
 	// urls 는 slice 이다
 	urls := []string{
 		"https://www.google.com/",
@@ -14,24 +25,24 @@ func main() {
 		"https://www.naver.com/",
 		"https://hwjskt.github.io/",
 	}
+
 	// 인덱스를 _ 처리하고 value를 출력한다
 	for _, url := range urls {
-		// fmt.Println(url)
-		hitURL(url)
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-var errRequestFailed = errors.New("request failed")
-
 // url을 클릭하면 결과가 어떻게나오는지 출력한다
 // golang std library 에서 net / http 모듈사용법을 확인할수있다
-func hitURL(url string) error {
-
+func hitURL(url string, c chan<- requestResult) {
+	fmt.Println("checking: ", url)
 	resp, err := http.Get(url)
-	fmt.Println("Checking: ", url, " --> Result: ", resp.StatusCode)
-	// StatusCode 400부터는 오류가 있는 경우
-	if err == nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+	var status = "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
